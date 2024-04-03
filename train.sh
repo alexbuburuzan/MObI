41355944c473335fb116b14eb6376da8743e7f3d
Alexandru Buburuzan <alexandru.buburuzan@five.ai>
2024-04-03 20:46:08 +0000

    Add muti-GPU support to training script

--- train.sh
+++ train.sh
@@ -2,4 +2,5 @@ python -u main.py \
 --logdir models/Paint-by-Example \
 --pretrained_model checkpoints/model.ckpt \
 --base configs/nusc_control.yaml \
---scale_lr False
+--scale_lr False \
+--gpus "0,1,2,3,4,5,6,7" \
