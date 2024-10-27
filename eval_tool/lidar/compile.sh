1e969c894e5d6d38a27e693fa4b45b7d922098a7
Alexandru Buburuzan <buburuzan08@gmail.com>
2024-10-27 16:51:54 +0000

    Add new FRID eval script

--- eval_tool/lidar/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd modules/chamfer
-python setup.py build_ext --inplace
-
-cd ../emd
-python setup.py build_ext --inplace
-
-cd ..
--- eval_tool/lidar/frd_score.py
+++ eval_tool/lidar/frd_score.py
@@ -1,66 +1,79 @@
-"""Calculates the Frechet Inception Distance (FID) to evalulate GANs
-The FID metric calculates the distance between two distributions of images.
-Typically, we have summary statistics (mean & covariance matrix) of one
-of these distributions, while the 2nd distribution is given by a GAN.
-When run as a stand-alone program, it compares the distribution of
-images that are stored as PNG/JPEG at a specified location with a
-distribution given by summary statistics (in pickle format).
-The FID is calculated by assuming that X_1 and X_2 are the activations of
-the pool_3 layer of the inception net for generated samples and real world
-samples respectively.
-See --help to see further details.
-Code adapted from https://github.com/bioinf-jku/TTUR to use PyTorch instead
-of Tensorflow
-Copyright 2018 Institute of Bioinformatics, JKU Linz
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-   http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 """
+Inspired from:
+@Author: Haoxi Ran
+@Date: 01/03/2024
+@Citation: Towards Realistic Scene Generation with LiDAR Diffusion Models
+"""
+
 import os
 import pathlib
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 
 import numpy as np
 import torch
-import torchvision.transforms as TF
-from PIL import Image
+import yaml
+from tqdm import tqdm
+import torch.nn.functional as F
 from scipy import linalg
-from torch.nn.functional import adaptive_avg_pool2d
 
-try:
-    from tqdm import tqdm
-except ImportError:
-    # If tqdm is not available, provide a mock version of it
-    def tqdm(x):
-        return x
+from rangenet.model import Model as rangenet
 
-class ImagePathDataset(torch.utils.data.Dataset):
-    def __init__(self, files, transforms=None):
-        self.files = files
-        self.transforms = transforms
+parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+parser.add_argument('--batch-size', type=int, default=64,
+                    help='Batch size to use')
+parser.add_argument('--num-workers', type=int,
+                    help=('Number of processes to use for data loading. '
+                          'Defaults to `min(8, num_cpus)`'))
+parser.add_argument('--device', type=str, default=None,
+                    help='Device to use. Like cuda, cuda:0 or cpu')
+parser.add_argument('--path-target', type=str, default=None,
+                    help='Path to target range views')
+parser.add_argument('--path-pred', type=str, default=None,
+                    help='Path to edited range views')
+
+class RangePathDataset(torch.utils.data.Dataset):
+    def __init__(
+        self,
+        path,
+        depth_interval = (1.4, 54),
+    ):
+        path = pathlib.Path(path)
+        self.files = list(path.glob('*.npy'))
+        self.depth_interval = depth_interval
 
     def __len__(self):
         return len(self.files)
 
     def __getitem__(self, i):
-        path = self.files[i]
-        img = Image.open(path).convert('RGB')
-        if self.transforms is not None:
-            img = self.transforms(img)
-        return img
+        file_path = self.files[i]
+        range_data = np.load(file_path)
+        range_depth, range_int, pitch, yaw = range_data[0], range_data[1], range_data[2], range_data[3]
+
+        # unnorm range depth
+        range_depth = (range_depth + 1) / 2 * self.depth_interval[1]
+        valid_mask = np.logical_and(range_depth > self.depth_interval[0], range_depth < self.depth_interval[1])
+
+        range_xyz = -np.ones((3, *range_depth.shape))
+        range_xyz[0] = np.cos(yaw) * np.cos(pitch) * range_depth
+        range_xyz[1] = -np.sin(yaw) * np.cos(pitch) * range_depth
+        range_xyz[2] = np.sin(pitch) * range_depth
+
+        range_data = np.concatenate(
+            [range_depth[None, :], range_int[None, :], range_xyz],
+            axis=0
+        )
+
+        range_data[:, ~valid_mask] = -1
+        range_data = torch.tensor(range_data).float()
+        range_data = F.interpolate(range_data.unsqueeze(1), size=(64, 1024), mode='nearest').squeeze()
+
+        return range_data
 
 
-def get_activations(files, model, batch_size=50, dims=2048, device='cpu',
-                    num_workers=1):
+def get_activations_of_path(path, model, batch_size=50, device='cpu', num_workers=1):
     """Calculates the activations of the pool_3 layer for all images.
     Params:
-    -- files       : List of image files paths
+    -- path        : Path to range views
     -- model       : Instance of inception model
     -- batch_size  : Batch size of images for the model to process at once.
                      Make sure that the number of samples is a multiple of
@@ -76,20 +89,15 @@ def get_activations(files, model, batch_size=50, dims=2048, device='cpu',
        query tensor.
     """
     model.eval()
-
-    if batch_size > len(files):
-        print(('Warning: batch size is bigger than the data size. '
-               'Setting batch size to data size'))
-        batch_size = len(files)
-
-    dataset = ImagePathDataset(files, transforms=TF.ToTensor())
+    print(path)
+    dataset = RangePathDataset(path)
     dataloader = torch.utils.data.DataLoader(dataset,
                                              batch_size=batch_size,
                                              shuffle=False,
                                              drop_last=False,
                                              num_workers=num_workers)
 
-    pred_arr = np.empty((len(files), dims))
+    pred_arr = np.empty((len(dataset), 512))
 
     start_idx = 0
 
@@ -97,17 +105,9 @@ def get_activations(files, model, batch_size=50, dims=2048, device='cpu',
         batch = batch.to(device)
 
         with torch.no_grad():
-            pred = model(batch)[0]
-
-        # If model output is not scalar, apply global spatial average pooling.
-        # This happens if you choose a dimensionality not equal 2048.
-        if pred.size(2) != 1 or pred.size(3) != 1:
-            pred = adaptive_avg_pool2d(pred, output_size=(1, 1))
-
-        pred = pred.squeeze(3).squeeze(2).cpu().numpy()
+            pred = model(batch, return_final_logits=True, agg_type="depth")
 
         pred_arr[start_idx:start_idx + pred.shape[0]] = pred
-
         start_idx = start_idx + pred.shape[0]
 
     return pred_arr
@@ -167,16 +167,15 @@ def calculate_frechet_distance(mu1, sigma1, mu2, sigma2, eps=1e-6):
             + np.trace(sigma2) - 2 * tr_covmean)
 
 
-def calculate_activation_statistics(files, model, batch_size=50, dims=2048,
-                                    device='cpu', num_workers=1):
+def compute_statistics_of_path(path, model, batch_size=64,
+                               device='cpu', num_workers=1):
     """Calculation of the statistics used by the FID.
     Params:
-    -- files       : List of image files paths
+    -- path        : Path to files
     -- model       : Instance of inception model
     -- batch_size  : The images numpy array is split into batches with
                      batch size batch_size. A reasonable batch size
                      depends on the hardware.
-    -- dims        : Dimensionality of features returned by Inception
     -- device      : Device to run calculations
     -- num_workers : Number of parallel dataloader workers
     Returns:
@@ -185,7 +184,52 @@ def calculate_activation_statistics(files, model, batch_size=50, dims=2048,
     -- sigma : The covariance matrix of the activations of the pool_3 layer of
                the inception model.
     """
-    act = get_activations(files, model, batch_size, dims, device, num_workers)
+    act = get_activations_of_path(path, model, batch_size, device, num_workers)
     mu = np.mean(act, axis=0)
     sigma = np.cov(act, rowvar=False)
     return mu, sigma
+
+def build_model(model_folder, device):
+    config = yaml.safe_load(open(os.path.join(model_folder, 'config.yaml'), 'r'))
+    model = rangenet(config)
+    model.load_pretrained_weights(model_folder)
+
+    model.to(device)
+    model.eval()
+
+    return model
+
+def calculate_frid_given_paths(path_target, path_pred, batch_size, device, num_workers=1):
+    model = build_model("./eval_tool/lidar/rangenet", device=device)
+
+    m1, s1 = compute_statistics_of_path(path_target, model, batch_size,
+                                        device, num_workers)
+    m2, s2 = compute_statistics_of_path(path_pred, model, batch_size,
+                                        device, num_workers)
+    frid_value = calculate_frechet_distance(m1, s1, m2, s2)
+
+    return frid_value
+
+def main():
+    args = parser.parse_args()
+
+    if args.device is None:
+        device = torch.device('cuda' if (torch.cuda.is_available()) else 'cpu')
+    else:
+        device = torch.device(args.device)
+
+    if args.num_workers is None:
+        num_avail_cpus = len(os.sched_getaffinity(0))
+        num_workers = min(num_avail_cpus, 8)
+    else:
+        num_workers = args.num_workers
+
+    score = calculate_frid_given_paths(
+        args.path_target, args.path_pred, args.batch_size,
+        device, num_workers
+    )
+
+    print("FRD: ", score)
+
+if __name__ == "__main__":
+    main()
