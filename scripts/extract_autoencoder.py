e6857dee8fa6be61cc6484c975c581e9ffa509c7
Alexandru Buburuzan <buburuzan08@gmail.com>
2026-01-19 00:15:49 +0100

    Refactor configs

--- scripts/extract_autoencoder.py
+++ scripts/extract_autoencoder.py
@@ -2,7 +2,7 @@ import torch
 from omegaconf import OmegaConf
 from scripts.inference_test_bench import load_model_from_config
 
-config = OmegaConf.load("configs/nusc_control_multimodal.yaml")
+config = OmegaConf.load("configs/mobi_nusc_all-classes_512.yaml")
 model = load_model_from_config(config, "checkpoints/model.ckpt")
 
 state_dict = model.first_stage_model.state_dict()
